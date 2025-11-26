7de41d85e5d868b58dc2164d9dfa6788acf6462a
Monica-Luna <monicaluna.84@gmail.com>
2025-11-26 10:57:35 +0100

    En Screen1 dos TextFormField con controller cada uno

--- practica_login_password/lib/presentation/screens/screen1.dart
+++ practica_login_password/lib/presentation/screens/screen1.dart
@@ -2,23 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:practica_login_password/presentation/screens/screen2.dart';
 
 class Screen1 extends StatelessWidget {
-
-  const Screen1({super.key});
+   final user = TextEditingController();
+   final password = TextEditingController();
+  
+  Screen1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Introduce tu usuario y contaseña")),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => Screen2()),
-            );
-          },
-          child: Text("Ir a la segunda pantalla"),
+          child: Padding(
+            padding: EdgeInsets.all(30),
+
+            child: Column(
+
+            children: [
+              TextFormField(
+                controller: user,
+                decoration: InputDecoration(labelText: "Usuario"),
+              ),
+
+              SizedBox(height: 20),
+
+              TextFormField(
+                controller: password,
+                decoration: InputDecoration(labelText: "Contraseña"),
+              ),
+
+              SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  if (user.text == password.text) {
+                    final user1 = user.text;
+
+                    user.clear();
+                    password.clear();
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => Screen2(),
+                      ),
+                    );
+
+                  } else {
+                    showDialog(                
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text("Error"),
+                        content: Text("Usuario y contraseña NO coinciden"),
+                        actions: [
+                          TextButton(
+                            child: Text("OK"),
+                            onPressed: () => Navigator.pop(context),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                },
+                child: Text("Entrar"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
